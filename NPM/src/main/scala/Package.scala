59980336ad8c6587a1da4af877fd68c86407061f
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2021-11-28 13:16:54 +0100

    Update Package.scala
    better to put this function in my class as a method

--- NPM/src/main/scala/Package.scala
+++ NPM/src/main/scala/Package.scala
@@ -1,1 +1,17 @@
-case class Package(Name: String = "Empty", Dependencies: List[Int] = List(), Version: List[String] = List())
+case class Package(Name: String = "Empty", Dependencies: List[Int] = List(), Version: List[String] = List()){
+  def get_json {
+    val url = s"https://registry.npmjs.org/${Name}"
+    val response = requests.get(url)
+    if (response.statusCode == 200) {
+      val json = ujson.read(response.text)
+      val versions = json.obj("versions").obj.toList
+      print(versions)
+      for ((version, remain) <- versions){
+        Version.appended(version)
+      }
+    }
+    else {
+      println(response.statusCode)
+    }
+  }
+}
