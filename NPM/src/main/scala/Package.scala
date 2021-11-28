449247dbec1ce34a218c808bf54cbc69d0c017b7
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2021-11-28 14:30:32 +0100

    Update Package.scala
    append didnt work in lists so i changed it to ListBuffer

--- NPM/src/main/scala/Package.scala
+++ NPM/src/main/scala/Package.scala
@@ -1,17 +1,20 @@
-case class Package(Name: String = "Empty", Dependencies: List[Int] = List(), Version: List[String] = List()){
-  def get_json {
+import scala.collection.mutable.ListBuffer
+
+case class Package(Name: String = "Empty", Dependencies: ListBuffer[Int] = ListBuffer[Int]()
+                   , Version: ListBuffer[String] = ListBuffer[String]()){
+  def get_json_and_versions: Package = {
     val url = s"https://registry.npmjs.org/${Name}"
     val response = requests.get(url)
     if (response.statusCode == 200) {
       val json = ujson.read(response.text)
       val versions = json.obj("versions").obj.toList
-      print(versions)
       for ((version, remain) <- versions){
-        Version.appended(version)
+        Version += version
       }
     }
     else {
       println(response.statusCode)
     }
+    return this
   }
 }
